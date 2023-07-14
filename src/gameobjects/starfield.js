31ebe004b1b07df118497708320098f744c8e064
tiboitel <tiboitel@student.42.fr>
2023-07-14 16:40:03 +0200

    [BUGFIX] (v0.0.5) Correction of a bugs that's causes starfield to not update star position correctly. Stars are now Graphics instead of simply canvas rendering shape.

--- src/gameobjects/starfield.js
+++ src/gameobjects/starfield.js
@@ -1,61 +1,59 @@
 class Starfield {
     constructor(scene, density, starSpeed, maxStarSize, depth) {
-        this.scene = scene;
-        this.density = density;
-        this.starSpeed = starSpeed;
-        this.maxStarSize = maxStarSize;
-        this.depth = depth;
-        this.stars = this.createStarfield();
+      this.scene = scene;
+      this.density = density;
+      this.starSpeed = starSpeed;
+      this.maxStarSize = maxStarSize;
+      this.depth = depth;
+      this.stars = this.createStarfield();
     }
-
+  
     update() {
-        const stars = this.stars.getChildren();
-
-         // Starfield update.
-        for (let i = 0; i < stars.length; i++) {
-            const star = stars[i];
-
-            // Scroll the star down by STAR_SPEED
-            star.y += this.starSpeed * 500 / 1000;
-
-            // If the star goes below the screen, reset its position to the top
-            if (star.y >= this.scene.game.config.height) {
-                star.y = 0;
-                star.update();
-            }
+      this.stars.forEach(star => {
+        star.y += this.starSpeed * 500 / 1000;
+  
+        if (star.y >= this.scene.game.config.height) {
+          star.y = 0;
+          star.x = Phaser.Math.Between(0, this.scene.game.config.width);
+          star.setRandomColor();
         }
+      });
     }
-
+  
     createStar(x, y) {
-        const size = Phaser.Math.Between(1, this.maxStarSize);
+      const size = Phaser.Math.Between(1, this.maxStarSize);
+      const star = this.scene.add.graphics();
+  
+      star.x = x;
+      star.y = y;
+      star.depth = this.depth;
+      star.fillStyle(0xffffff);
+  
+      star.setRandomColor = function() {
         const color = Phaser.Display.Color.HexStringToColor('#' + Phaser.Math.RND.integerInRange(0x37E2D5, 0XFBCB0A).toString(16));
-        const alpha = Phaser.Math.FloatBetween(0.2, 0.8);
-        const shape = Phaser.Math.Between(0, 1); // 0 = cross, 1 = circle
-        const star = this.scene.add.graphics();
-
-        star.fillStyle(color.color, alpha);
-        if (shape === 0) {
-            star.fillRect(x - size / 2, y - size / 4, size, size / 2);
-            star.fillRect(x - size / 4, y - size / 2, size / 2, size);
-        } else {
-            star.fillCircle(x, y, size);
-        }
-
-        return star;
+        star.clear();
+        star.fillStyle(color.color);
+        star.fillRect(-size / 2, -size / 4, size, size / 2);
+        star.fillRect(-size / 4, -size / 2, size / 2, size);
+      };
+  
+      star.setRandomColor();
+  
+      return star;
     }
-
+  
     createStarfield() {
-        const stars = this.scene.add.group();
-
-        for (let y = 0; y < this.scene.sys.game.config.height; y += 20) {
-            for (let i = 0; i < Phaser.Math.Between(this.density / 2, this.density * 2); i++) {
-                const x = Phaser.Math.Between(0, this.scene.sys.game.config.width);
-                const star = this.createStar.call(this, x, y);
-                star.setDepth(this.depth);
-                stars.add(star);
-            }
+      const stars = [];
+  
+      for (let y = 0; y < this.scene.sys.game.config.height; y += 20) {
+        for (let i = 0; i < Phaser.Math.Between(this.density / 2, this.density * 2); i++) {
+          const x = Phaser.Math.Between(0, this.scene.sys.game.config.width);
+          const star = this.createStar(x, y);
+          stars.push(star);
         }
-
-        return stars;
+      }
+  
+      return stars;
     }
-}
+  }
+  
